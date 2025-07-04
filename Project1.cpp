08b7514d7d07396ee4a80f279eb8c5d06874867a
Nik <semen.harit@mail.ru>
2025-07-04 14:03:11 +0700

    Project1

---
+++ Project1.cpp
@@ -0,0 +1,21 @@
+﻿#include <FL/Fl.H>
+
+#include <FL/Fl_Window.H>
+
+#include <FL/Fl_Box.H>
+
+#include <FL/Fl_Button.H>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	Fl_Window win(800, 600, /*u8*/"Главное окно");
+	win.end();
+
+	win.show(argc, argv);
+	return Fl::run();
+
+
+
+}
